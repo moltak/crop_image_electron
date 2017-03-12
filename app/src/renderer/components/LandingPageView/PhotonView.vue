fc04d51f0306d65d720149196ca0aabd1df60a14
moltak <moltak@gmail.com>
2017-03-12 10:02:58 +0000

    created base view with Photon
    what happened
    
    [ticket: X]

---
+++ app/src/renderer/components/LandingPageView/PhotonView.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div(class="pane-group")
+    div(class="pane-sm sidebar")
+      h1 sidebar
+    div(class="pane")
+      h1 pane
+</template>
+
+<script>
+  export default {
+    name: 'PhotonView',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ app/src/renderer/components/LandingPageView/TopBottomBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="window">
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Header</h1>
+    </header>
+    <div class="window-content">
+      <div class="pane-group">
+        <div class="pane-sm sidebar">sidebar</div>
+        <div class="pane">pane</div>
+      </div>
+    </div>
+    <footer class="toolbar toolbar-footer">
+      <h1 class="title">Footer</h1>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'topbottom-bar'
+  }
+</script>
+
+<style scoped>
+</style>

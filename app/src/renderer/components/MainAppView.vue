b07d4d964b33726fde0cc660b13f81662c1ce631
moltak <moltak@gmail.com>
2017-03-12 14:21:47 +0000

    Rename LandingPageView -> MainAppView, Move asset dir
    what happened
    
    [ticket: X]

---
+++ app/src/renderer/components/MainAppView.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div
+    TopBottomBar
+</template>
+
+<script>
+  import TopBottomBar from './MainAppView/TopBottomBar'
+
+  export default {
+    components: {
+      TopBottomBar
+    },
+    name: 'main'
+  }
+</script>
+
+<style scoped>
+  @import url("./assets/photon-0.1.2/css/photon.css");
+</style>
